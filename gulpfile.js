2e33b3470a59d9fecc2e51d12bd029150cdc862c
Dan Addison <danaddisoncreative@gmail.com>
2018-11-13 13:52:20 +0000

    Refactor js structure and include page-specific scripts in 'js' block

--- gulpfile.js
+++ gulpfile.js
@@ -60,8 +60,8 @@ gulp.task('useref', function() {
 
   return gulp.src('src/*.html')
     .pipe(useref())
-    .pipe(gulpIf('*.js', uglify())) // do I need assets/ path here?
-    .pipe(gulpIf('*.css', cssnano())) // do I need assets/ path here?
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
 });
 
